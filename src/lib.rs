d4992b7ddad5f682df060a97f331bd3ff536dc1b
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-02-19 14:00:34 +0000

    feat: design types for formula and tableau

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
+use std::collections::HashSet;
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum SolverError {
     EmptyFormula,
+    IllFormedFormula,
+    UnknownError,
 }
 
 pub fn is_satisfiable(formula: &str) -> Result<bool, SolverError> {
@@ -8,6 +12,79 @@ pub fn is_satisfiable(formula: &str) -> Result<bool, SolverError> {
         return Err(SolverError::EmptyFormula);
     };
 
+    let formula = formula.trim();
+
+    let tableau = match build_tableau(formula) {
+        Some(tab) => tab,
+        None => {
+            return Err(SolverError::IllFormedFormula);
+        }
+    };
+
+    unimplemented!()
+}
+
+#[derive(Clone)]
+struct Tableau {
+    theories: Vec<Theory>,
+}
+
+impl Tableau {
+    fn new() -> Tableau {
+        Tableau {
+            theories: Vec::new(),
+        }
+    }
+}
+
+#[derive(Clone)]
+struct Theory {
+    formulas: HashSet<PropositionalFormula>,
+}
+
+impl Theory {
+    fn new() -> Theory {
+        Theory {
+            formulas: HashSet::new()
+        }
+    }
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+enum PropositionalFormula {
+    Variable(String),
+    Negation(Box<PropositionalFormula>),
+    Conjunction(Conjunction),
+    Disjunction(Disjunction),
+    Implication(Implication),
+    Biimplication(Biimplication),
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+struct Conjunction {
+    left: Box<PropositionalFormula>,
+    right: Box<PropositionalFormula>,
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+struct Disjunction {
+    left: Box<PropositionalFormula>,
+    right: Box<PropositionalFormula>,
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+struct Implication {
+    premise: Box<PropositionalFormula>,
+    conclusion: Box<PropositionalFormula>,
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+struct Biimplication {
+    left: Box<PropositionalFormula>,
+    right: Box<PropositionalFormula>,
+}
+
+fn build_tableau(_formula: &str) -> Option<Tableau> {
     unimplemented!()
 }
 
