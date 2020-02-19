e4be9f5063dc19f5360725ffcde6ea414ee23441
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-02-19 13:23:25 +0000

    feat: handle empty formula

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum SolverError {
+    EmptyFormula,
+}
+
+pub fn is_satisfiable(formula: &str) -> Result<bool, SolverError> {
+    if formula.is_empty() {
+        return Err(SolverError::EmptyFormula);
+    };
+
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_string() {
+        let result = is_satisfiable("");
+        assert!(result.is_err());
+        assert_eq!(SolverError::EmptyFormula, result.err().unwrap());
+    }
+}
+
