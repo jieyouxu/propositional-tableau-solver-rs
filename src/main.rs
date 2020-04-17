bac5c46970b897e69f9afae52471785506b74ab4
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-04-17 19:37:54 +0800

    Restart to use parser combinator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use colored::*;
+
+const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
+
+pub fn main() {
+    println!(
+        "{}: v{}",
+        "Propositional Tableau Solver".cyan().bold().underline(),
+        VERSION.unwrap_or("unknown version").yellow()
+    );
 }
