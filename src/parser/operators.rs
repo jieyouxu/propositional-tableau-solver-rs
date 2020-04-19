049f9ba8a2f1f59f07b029d8231e19167203744f
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-04-19 17:45:53 +0800

    refactor: use `value` combinator
    The `adapter` and `adapter_no_input` functions are re-inventing the `value`
    combinator.

--- src/parser/operators.rs
+++ src/parser/operators.rs
@@ -1,64 +1,27 @@
 //! Parser combinators for unary and binary operators.
 
 use super::ParseResult;
-use crate::formula::operators::{BinaryOperator, Operator, UnaryOperator};
+use crate::formula::{BinaryOperator, UnaryOperator};
 
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete::char;
-use nom::combinator::map;
+use nom::combinator::value;
 
 /// Parses a unary operator.
 pub fn unary_operator(input: &str) -> ParseResult<&str, UnaryOperator> {
-    let negation = char('-');
-    map(negation, |_| UnaryOperator::Negation)(input)
+    let negation = value(UnaryOperator::Negation, char('-'));
+    negation(input)
 }
 
 /// Parses a binary operator.
 pub fn binary_operator(input: &str) -> ParseResult<&str, BinaryOperator> {
-    let and = adapter_ignore_input(char('^'), BinaryOperator::And);
-    let or = adapter_ignore_input(char('|'), BinaryOperator::Or);
-    let implication = adapter_ignore_input(tag("->"), BinaryOperator::Implication);
-    let biimplication = adapter_ignore_input(tag("<->"), BinaryOperator::Biimplication);
+    let and = value(BinaryOperator::And, char('^'));
+    let or = value(BinaryOperator::Or, char('|'));
+    let implication = value(BinaryOperator::Implication, tag("->"));
+    let biimplication = value(BinaryOperator::Biimplication, tag("<->"));
 
     alt((and, or, implication, biimplication))(input)
-}
-
-/// Converts a parser of the form (for some original output type `C`):
-///
-/// ```no_run
-/// Fn(I) -> ParseResult<I, C>
-/// ```
-///
-/// into the desired form (for some desired output type `R`):
-///
-/// ```no_run
-/// Fn(I) -> ParseResult<I, R>
-/// ```
-///
-/// Given that a mapping function `M: Fn(C) -> R` is provided.
-#[inline]
-fn adapter<I, C, R, M, F>(original: F, mapping_fn: M) -> impl Fn(I) -> ParseResult<I, R>
-where
-    F: Fn(I) -> ParseResult<I, C>,
-    M: Fn(C) -> R,
-{
-    map(original, mapping_fn)
-}
-
-/// Specialization of the `adapter_helper` where the value is independent from the input string.
-#[inline]
-fn adapter_ignore_input<'a, I, C, R, F>(
-    original: F,
-    value: R,
-) -> impl Fn(I) -> ParseResult<I, R> + 'a
-where
-    I: 'a,
-    C: 'a,
-    R: Copy + 'a,
-    F: Fn(I) -> ParseResult<I, C> + 'a,
-{
-    adapter(original, move |_| value)
 }
 
 #[cfg(test)]
